0aea4fb9ad3064462adfaabbb386251e68ebde03
raxxaNt <programracb@gmail.com>
2023-11-16 17:11:33 +0100

    Update of Practise

--- Practicas.c
+++ Practicas.c
@@ -37,6 +37,50 @@ void convert_pes_en_eur() //Convierte Euros en Pesetas
     printf("\nLa cantidad de Euros en Pesetas es de: %.2lf\n", pesetas);
 }
 
+void check_note() //El 'alumno' introduce una nota y comprueba cuál ha sido su calificación
+{
+    float nota;
+
+    printf("Introduce tu nota para saber tu calificación: ");
+    scanf("%f", &nota);
+    if (nota < 5 && nota >= 0)
+        printf("\nLo siento, has suspendido\n");
+    else if (nota >= 5 && nota < 6)
+        printf("\n¡Has obtenido un suficiente!\n");
+    else if (nota >= 6 && nota < 7)
+        printf("\n¡Has obtenido un bien!\n");
+    else if (nota >= 7 && nota < 9)
+        printf("\n¡Has obtenido un notable!\n");
+    else if (nota >= 9 && nota <= 10)
+        printf("\n¡Felicidades!, ¡tienes un sobresaliente!\n");
+    else
+        printf("\nLa nota que has introducido no es una calificación válida.\n");
+}
+
+void final_price() //Calcula el precio final de un coche basándose en los parámetros introducidos (Precio base, km totales y consumo)
+{
+    float base_price, avrg_cons, tot_price;
+    int tot_km;
+
+    printf("Introduce el precio base del vehículo: ");
+    scanf("%f", &base_price);
+    printf("Introduce el número de kilómetros del vehículo: ");
+    scanf("%d", &tot_km);
+    printf("Introduce el consumo medio del vehículo: ");
+    scanf("%f", &avrg_cons);
+
+    if (tot_km < 20000 && avrg_cons <= 5)
+        tot_price = base_price * 1.20;
+    else if (tot_km >= 20000 && avrg_cons <= 5)
+        tot_price = base_price *1.10;
+    else if (tot_km < 20000 && avrg_cons > 5)
+        tot_price = base_price * 1.25;
+    else {
+        tot_price = base_price * 1.15;
+    }
+    printf("\nEl precio final del coche es de: %.2f\n", tot_price);
+}
+
 int main() {
     //No necesito pedir parametros en las funciones debido a que depende del usuario mediante 'scanf'
     area_rectangulo();
@@ -44,6 +88,10 @@ int main() {
     tres_caracteres();
     printf("\n");
     convert_pes_en_eur();
+    printf("\n");
+    check_note();
+    printf("\n");
+    final_price();
     printf("\n");
     return 0;
 }
